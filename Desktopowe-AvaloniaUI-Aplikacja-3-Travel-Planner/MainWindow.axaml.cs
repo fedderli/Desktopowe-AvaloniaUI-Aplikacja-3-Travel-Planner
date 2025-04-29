b4bd5c191b6a8f63a84aa60f68e3abf94745679f
karol <kapo@com.pl>
2025-04-29 09:07:55 +0200

    first commit

---
+++ Desktopowe-AvaloniaUI-Aplikacja-3-Travel-Planner/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Desktopowe_AvaloniaUI_Aplikacja_3_Travel_Planner;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
